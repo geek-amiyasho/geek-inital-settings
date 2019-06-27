95b317c50c3a416932077a24606351ff69339db1
geek-amiyasho <amiyasho@geek.co.jp>
2019-06-27 10:40:02 +0900

    init

---
+++ brew.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+
+# https://formulae.brew.sh/cask/
+# https://scrapbox.io/geek/Macbook_%E7%92%B0%E5%A2%83%E6%A7%8B%E7%AF%89_%E6%A8%99%E6%BA%96%E5%8C%96
+
+
+# cli
+brew install asciidoctor
+brew install node
+brew install tmux
+brew install wget
+brew install git
+brew install vim
+brew install yarn
+brew install fish
+brew install ripgrep
+
+# Terminal
+brew cask install iterm2
+
+# browser
+brew cask install firefox
+brew cask install google-chrome
+
+# design
+brew cask install zeplin
+brew cask install figma
+
+# editor
+brew cask install visual-studio-code
+brew cask install coteditor
+
+# other
+brew cask install docker
+brew cask install postman
+
+# communication
+brew cask install skype
+brew cask install gyazo
+brew cask install google-japanese-ime
+brew cask install slack
+brew cask install zoomus
+
+
